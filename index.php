a3a682fe753026316ab8e1b7cac82d8ea93f1029
Julia Simanova <simanova.julya@yandex.ru>
2017-07-21 11:59:51 +0300

    correction

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 echo "Задание #1", "<br />", "<br />";
@@ -6,12 +5,11 @@ echo "Задание #1", "<br />", "<br />";
 $name = "Юлия";
 $age = 21;
 echo "Меня зовут: $name.", "<br />";
-echo  "Мне $age год.", "<br />";
+echo "Мне $age год.", "<br />";
 echo "“!|\/’”\"", "<br />", "<br />";
 
 
 echo "Задание #2", "<br />", "<br />";
-
 
 $x = "17";
 $y = "80";
@@ -23,69 +21,123 @@ echo "Решение: $y-$z-$e=$x рисунков выполненно крас
 
 echo "Задание #3", "<br />", "<br />";
 
-define ("k", 8.98, true);
-if (defined ("k")==true) echo "Константа k объявлена!", "<br />", "<br />";
-define("k",8.97,true);
+define ("K", 8.9, true);
+if (defined ("K") == true)
+    echo "Константа K объявлена!", "<br />", "<br />";
+define ("K", 8.8, true);
+
 
 echo "Задание #4", "<br />", "<br />";
 
 $age = 21;
-if ($age>=18 and $age<=65) { echo "Вам   еще работать   и   работать" ;}
-elseif ($age>65) { echo "Вам пора на пенсию";}
-elseif ($age>=1 and $age<=17) { echo "Вам еще рано работать";}
-else { echo "Неизвестный возраст";}
-echo  "<br />", "<br />";
+if ($age >= 18 and $age <= 65) {
+    echo "Вам   еще работать   и   работать";
+} elseif ($age > 65) {
+    echo "Вам пора на пенсию";
+} elseif ($age >= 1 and $age <= 17) {
+    echo "Вам еще рано работать";
+} else {
+    echo "Неизвестный возраст";
+}
+echo "<br />", "<br />";
 
 echo "Задание #5", "<br />", "<br />";
 $day = 3;
 switch ($day) {
     case 1:
     case 2:
-    case 3: echo "Это рабочий день"; break;
+    case 3:
+        echo "Это рабочий день";
+        break;
     case 4:
     case 5:
-    case 6: echo "Это выходной день"; break;
-    case 7: echo "Это выходной день"; break;
-    default: echo "Это неизвестный день";
+    case 6:
+        echo "Это выходной день";
+        break;
+    case 7:
+        echo "Это выходной день";
+        break;
+    default:
+        echo "Это неизвестный день";
 }
 echo "<br />", "<br />";
 
 echo "Задание #6", "<br />", "<br />";
 
-$bmw = array ( "model" => "X5", "speed"=>"120", "doors"=>"5", "year"=>"2015");
-$toyota = array ( "model" => "Corolla", "speed"=>"140", "doors"=>"5", "year"=>"2006");
-$opel = array ( "model" => "Zafira", "speed"=>"110", "doors"=>"5", "year"=>"2010");
+echo "CAR", "<br />", "<br />" ;
+$bmw = array ("model" => "X5", "speed" => "120", "doors" => "5", "year" => "2015");
+$toyota = array ("model" => "Corolla", "speed" => "140", "doors" => "5", "year" => "2006");
+$opel = array ("model" => "Zafira", "speed" => "110", "doors" => "5", "year" => "2010");
 $car = array ();
-$car [] = $bmw;
-$car [] = $toyota;
-$car [] = $opel;
-echo "<br>CAR bmw<br />"," ", $bmw["model"]," ",$bmw["speed"]," ",$bmw["doors"]," ",$bmw["year"],"<br />","</p>";
-echo "<br>CAR toyota<br />"," ", $toyota["model"]," ",$toyota["speed"]," ",$toyota["doors"]," ",$toyota["year"],"<br />","</p>";
-echo "<br>CAR opel<br />"," ", $opel["model"]," ",$opel["speed"]," ",$opel["doors"]," ",$opel["year"],"<br />","</p>";
-$result = array_merge($bmw, $toyota, $opel);
+$car ["bmw"] = $bmw;
+$car ["toyota"] = $toyota;
+$car ["opel"] = $opel;
+echo "bmw", " ";
+$car ["bmw"] = "";
+foreach ($bmw as $key => $value){
+
+    $car ["bmw"].= $value;
+    if ($key!= "year") {
+        $car ["bmw"].= "  ";
+    }
+}
+echo $car ["bmw"]. "<br>";
+echo "toyota", " ";
+$car ["toyota"] = "";
+foreach ($toyota as $key=>$value) {
+
+    $car ["toyota"].=  $value;
+    if ($key!= "year") {
+        $car ["toyota"].= "  ";
+    }
+}
+echo $car ["toyota"]. "<br>";
+echo "opel", " ";
+$car ["opel"] = "";
+foreach ($opel as $key=>$value) {
+
+    $car ["opel"].= $value;
+    if ($key!= "year") {
+        $car ["opel"].= "  ";
+    }
+}
+echo $car ["opel"]. "<br>";
+$result = array_merge ($bmw, $toyota, $opel);
 echo $result;
 echo "<br />", "<br />";
 
 echo "Задание #7", "<br />", "<br />";
 
-echo "<table><tr>";
-for ($i= 1; $i<= 10; $i++) {
-    echo "<tr>";
-    for ($j= 1; $j<=10; $j++)
-        echo "<td>".($i*$j)."</td>"; };
-echo "</table></tr>";
+echo "<table >";
+echo "<tr>";
+for ($i = 1; $i <= 10; $i++) {
+    for ($j = 1; $j <= 10; $j++) {
+        echo "<td>";
+        if ($i % 2 == 0 && $j % 2 == 0)
+            echo "(" . $j * $i . ")";
+        elseif ($i % 2 == 1 && $j % 2 == 1)
+            echo "[" . $j * $i . "]";
+        else echo($j * $i);
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
 echo "<br />", "<br />";
 
 echo "Задание #8", "<br />", "<br />";
 
-$str= "Hello World";
-echo $str;
-$parts = explode(":", $str);
-echo "<br />";
-echo $parts [0];
-echo $parts [1];
-while ($parts [0]+ $parts [1]) echo $str;
-echo "<br />", "<br />";
+$str = "Hello World !";
+echo $str . '<br>';
+$arr = explode (" ", $str);
+echo "<p>" . print_r ($arr, true) . "</p>";
+$parts = count ($arr);
+while ($parts) {
+    $newArr[] = $arr [$parts - 1];
+    $parts--;
+}
+$new = implode (' : ', $newArr);
+echo $new;
 
 
 
